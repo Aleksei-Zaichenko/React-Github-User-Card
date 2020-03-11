f2ae60768218af5023434596c94a97dee337904e
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-03-10 19:23:12 -0500

    finished basic structure started to work on design

--- githugcards/src/App.js
+++ githugcards/src/App.js
@@ -20,31 +20,44 @@ class App extends React.Component {
         this.setState({
           cards: [...this.state.cards,res.data]
         })
+        
         axios
           .get('https://api.github.com/users/Aleksei-Zaichenko/followers')
           .then(res =>{
-            this.setState({
-              cards: [...this.state.cards, ...res.data]
+            res.data.forEach(item => {
+              axios
+                .get(item.url)
+                .then( response =>{
+                  this.setState({
+                    cards: [...this.state.cards, response.data]
+                  })
           })
-          console.log('cards', this.state.cards)
         })
           .catch(err => console.log(err.message))
       })
       .catch(err => console.log(err.message))
-  }
+  })
+}
 
   render(){
     return (
+      console.log('cards', this.state.cards),
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            I decided to keep React logo
-          </p>
-        </header>
         <div className="container">
           <div className="cards">
-
+            {
+              this.state.cards.map(account =>(
+                <div className ="card">
+                  <img src={account.avatar_url} />
+                  <h3>{account.name}</h3>
+                  <p>{account.login}</p>
+                  <p>Followers: {account.followers}</p>
+                  <p>Following: {account.following}</p>
+                  <p>Link to Github: {account.url}</p>
+                  <p>Biography: {account.bio}</p>
+                </div>
+              ))
+            }
           </div>{/*cards*/}
         </div>{/*container*/}
       </div>
--- githugcards/src/components/GithubCard.js
+++ githugcards/src/components/GithubCard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const GithubCard = () => {
+const GithubCard = props => {
+    console.log('props',props)
     return (
-        <div className='card'>
-            
-        </div>
+            <div>
+
+            </div>
     );
 };
 
